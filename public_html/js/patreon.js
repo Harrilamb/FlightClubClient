451038e1f199808ae3c99f3131ec50e22e9b34e6
Declan <murphd37@tcd.ie>
2016-12-23 02:03:46 -0600

    styled patreon page

--- public_html/js/patreon.js
+++ public_html/js/patreon.js
@@ -3,36 +3,16 @@ angular.module('FlightClub').controller('PatreonCtrl', function ($scope, $cookie
     $scope.$emit('viewBroadcast', 'patreon');
 
     $scope.loading = true;
-    //$scope.$parent.toolbarClass = 'hide';
-    $scope.rewards = [
-        {id: 1103003, name: "Suborbital"},
-        {id: 1103091, name: "Low Earth Orbit (LEO)"},
-        {id: 1101121, name: "Medium Earth Orbit (MEO)"},
-        {id: 1103002, name: "Geostationary Transfer Orbit (GTO)"},
-        {id: 1102991, name: "Geostationary Earth Orbit (GEO)"}
-    ];
-    $scope.chosenClass = "Geostationary Earth Orbit (GEO)";
+    $scope.$parent.toolbarTitle = 'Flight Club | Patreon';
     
-    var getRewardById = function(id) {
-        var ret = null;
-        $scope.rewards.forEach(function(el) {
-            if(el.id === id)
-                ret = el;
-        });
-        return ret;
-    };
+    var queryString = window.location.search.substring(1);
+    $scope.queryParams = $scope.parseQueryString(queryString);
         
     queryString = 'auth=' + $cookies.get($scope.$parent.cookies.AUTHTOKEN);
     $scope.httpRequest('/patreon/patrons?'+queryString, 'GET', null, function (data) {
         var json = data.data;
         if(json.Success) {
-            for (var key in json.data) {
-                if(json.data.hasOwnProperty(key)) {
-                    key = parseInt(key);
-                    var reward = getRewardById(key);
-                    reward.list = json.data[key];
-                }
-            }
+            $scope.rewards = json.data;
         } else {
             
         }
@@ -40,5 +20,11 @@ angular.module('FlightClub').controller('PatreonCtrl', function ($scope, $cookie
     }, function (data) {
         $scope.loading = false;        
     });
+    
+    $scope.show = function(tier) {
+        if($scope.queryParams===undefined)
+            return true;
+        return tier.name.indexOf($scope.queryParams.id)!==-1;
+    };
     
 });
